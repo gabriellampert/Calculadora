b3605faf6817c0012b2c01d6f7ae76bfe5889c78
gabriellampert <116816725+gabriellampert@users.noreply.github.com>
2023-05-18 10:57:07 -0300

    Sexto commit

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const validKeys = ["Delete", "Backspace", "Enter", "X", "x", "-", "/", ".", "+", 
+"7", "8", "9", "4", "5", "6", "1", "2", "3", "0"];
+
 function insert(num){
    var numero = document.getElementById('resultado').innerHTML;
    document.getElementById('resultado').innerHTML = numero + num;
@@ -13,10 +16,29 @@ function calcular(){
     var resultado = document.getElementById('resultado').innerHTML;
      if(resultado)
       {
-        document.getElementById('resultado').innerHTML = eval(resultado.replace("X", "*"));
+        document.getElementById('resultado').innerHTML = eval(resultado.replace(/[xX]/, "*"));
       }
       else
       {
         document.getElementById('resultado').innerHTML = "Nada...";
       }
 }
+
+
+
+window.addEventListener("keyup", ({ key }) => {
+  if (validKeys.some(keyFunc => key == keyFunc)) {
+    if (key == "Enter") {
+      calcular();
+    }
+    else if (key == "Delete") {
+      clean();
+    }
+    else if (key == "Backspace") {
+      back()
+    }
+    else {
+      insert(key);
+    }
+  }
+});
