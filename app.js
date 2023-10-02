919faafbf287ec19f69f5e66939fe79a7fcea604
fernandobarrosd <fdebarros0910-2004@hotmail.com>
2023-10-02 09:24:13 -0300

    Update app.js and index.html

--- app.js
+++ app.js
@@ -1,44 +1,98 @@
-const validKeys = ["Delete", "Backspace", "Enter", "X", "x", "-", "/", ".", "+", 
-"7", "8", "9", "4", "5", "6", "1", "2", "3", "0"];
+const resultadoElement = document.getElementById('resultado');
+
+const calculatorButtons = 
+document.querySelectorAll("button.botao");
+
+const symbolsActions = {
+  "+": () => insert("+"),
+  "-": () => insert("-"),
+  "/": () => insert("/"),
+  "X": () => insert("x"),
+  "0": () => insert("0"),
+  "1": () => insert("1"),
+  "2": () => insert("2"),
+  "3": () => insert("3"),
+  "4": () => insert("4"),
+  "5": () => insert("5"),
+  "6": () => insert("6"),
+  "7": () => insert("7"),
+  "8": () => insert("8"),
+  "9": () => insert("9")
+}
+
+const calculatorActions = {
+  buttons: {
+    "C": clean,
+    "<": back,
+    "=": calculate,
+    ...symbolsActions
+
+  },
+  keys: {
+    "ENTER": calculate,
+    "DELETE": clean,
+    "BACKSPACE": back,
+    ...symbolsActions
+   
+  }
+}
+
+
+calculatorButtons.forEach(calculatorButton => {
+  calculatorButton.addEventListener("click", e => {
+    if (e.target instanceof HTMLButtonElement) {
+      const buttonText = e.target.textContent;
+
+      console.log(buttonText);
+
+      const buttonFunction = 
+      calculatorActions.buttons[buttonText];
+
+      buttonFunction();
+      
+    }
+    
+  });
+});
+
+
+
+function updateResultado(text) {
+  resultadoElement.innerHTML = text;
+}
 
 function insert(num){
-   var numero = document.getElementById('resultado').innerHTML;
-   document.getElementById('resultado').innerHTML = numero + num;
+   var numero = resultadoElement.innerHTML;
+   updateResultado(numero + num);
 }
+
+
 function clean(){
-    document.getElementById('resultado').innerHTML = '';
+    updateResultado("");
 }
 function back(){
-    var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0,resultado.length -1);
+    var resultado = resultadoElement.innerHTML;
+    updateResultado(resultado.substring(0, resultado.length - 1));
+
 }
-function calcular(){
-    var resultado = document.getElementById('resultado').innerHTML;
-     if(resultado)
-      {
-        document.getElementById('resultado').innerHTML = eval(resultado.replace(/[xX]/, "*"));
+function calculate(){
+    var resultado = resultadoElement.innerHTML;
+    
+
+     if(resultado){
+        updateResultado(eval(resultado.replace(/[xX]/, "*")));
       }
-      else
-      {
+      else {
+        updateResultado("Nada...");
         document.getElementById('resultado').innerHTML = "Nada...";
       }
 }
 
 
-
 window.addEventListener("keyup", ({ key }) => {
-  if (validKeys.some(keyFunc => key == keyFunc)) {
-    if (key == "Enter") {
-      calcular();
-    }
-    else if (key == "Delete") {
-      clean();
-    }
-    else if (key == "Backspace") {
-      back()
-    }
-    else {
-      insert(key);
-    }
+  const keyFunc = calculatorActions.keys[key.toUpperCase()];
+
+  if (keyFunc) {
+    keyFunc();
   }
 });
